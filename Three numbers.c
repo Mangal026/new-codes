f880311cec0c9899d5f1a2f298363121dbf5543d
Mangal Pandey <mangal0078324@gmail.com>
2024-10-29 22:07:13 +0530

    Three numbers.c

--- Three numbers.c
+++ Three numbers.c
@@ -1,36 +1,31 @@
 //Code About which Number Aming the three is largest and which is smallest,to check odd even
-#include<stdio.h>
-int main(){
-  int a,b,c;
-printf("Enter:A,B,C:-->");
-scanf("%d%d%d,&a,&b,&c");
-  if(a>b && a>c){
-printf("A is largest:%d",a);
-     if(b>c){
-        printf("C is least:%d",c);
-            }
-     else{
-       printf("B is least:%d",b);
-         }
-}
-else{
-  if(b>c){
-    printf("B is largest:%d",b);
-      if(a>c){
-      printf("C is least:%d",c);
-         }
-      else{
-        printf("A is least:%d",a)
-      }
-  }
-    else{
-      printf("C is largest:%d",c);
-      if(a>b){
-        printf("B is least:%d",b);
-      }
-      else{
-        printf("C is least:%d",c);
-      }
+#include <stdio.h>
+
+int main()
+{
+ int a,b,c;
+ printf("Enter:");
+ scanf("%d %d %d",&a,&b,&c);
+ //larger number among A,B,C 
+ if(a>b&&a>c){
+     printf("A is largest:%d\n",a);
+ }
+ else if(b>c){
+ printf("B is larges:%d\n",b);
+     
+ }
+ else{
+     printf("C is largest:%d\n",c);
+ }
+ //Smallest Among the three
+ if(a<c&&a<b){
+    printf("A is smallest:%d\n",a);
+     }
+ else if(b<c){
+        printf("B is smallest:%d\n",b);
+        
     }
-    
-  }
+    else { printf("C is smallest:%d\n",c);
+    }
+    return 0;
+}
